0152403d66f2f21f18070b27f5efc43b79d0f3f3
Prashant <prash@qodemedia.com>
2019-04-10 21:08:45 +0545

    Code updated for CH6-Spread-Syntax

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Destructuring Assignment
+# Spread syntax
 
-Destructuring allows us to unpack object properties or array elements and assign them to distinct variables. Take the following object myObj. To assign its properties to new variables, we use the .(dot) notation.
+ES6 introduces a new syntax known as spread, represented by three periods (...) as a shortcut for expanding an object or an arrays. It allows us to expand an object or an array into a place where multiple key/value pairs are expected.
--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-/*
-let numbers = [10,20,30,40,50];
-let a,b;
-[a,b] = numbers;
-console.log(a);
-console.log(b);
-*/
+/* let numbers = [10,20,30,40,50];
+let newNumbers = [1,2,3,4,5,...numbers];
+console.log(newNumbers); */
 
-/*
-let c;
-[a,b, ...c] = numbers;
-console.log(c)
-*/
+/* function sum(x, y, z) {
+  return x + y + z;
+}
 
-/* 
-let myObj = {
-    prop1: 'Hello',
-    prop2: 'World'
-  };
- const { prop1, prop2 } = myObj; 
- console.log(prop1);  // Output: Hello 
- */
+const numbers = [1, 2, 3];
 
-/* let myObj = {
-    prop1: 'Hello',
-    prop2: 'World',
-    prop3: 'Kunwar',
-    prop4: 'Chandra',
-    prop5: 'Pal'
-  };
- const { prop1, prop2, ...name } = myObj; 
- console.log(prop1);
- console.log(name); */
- 
+console.log(sum(...numbers)); */
+
+/* var dateFields = [2019, 0, 1];  // 2019 Jan 1
+var d = new Date(...dateFields);
+console.log(d) */
+
+/* var parts = ['shoulders', 'knees']; 
+var lyrics = ['head', ...parts, 'and', 'toes']; 
+console.log(lyrics)
+// ["head", "shoulders", "knees", "and", "toes"] */
+
+/* var arr1 = [0, 1, 2];
+var arr2 = [3, 4, 5];
+arr1 = arr1.concat(arr2);
+arr1 = [...arr1, ...arr2]; */
+
+/* let myObj = {prop1: 'Hello', prop2: 'World'};
+let newObj = {
+   name: 'Peshal',
+   ...myObj
+ };
+console.log(`${newObj.prop1} ${newObj.prop2} ${newObj.name}`);  */
+
+/* let person = {name:'summit',age:30,height:"6'5",weight:70}
+let programmer = {name:'summit', language:'Python',level:'Ninja'}
+let personAsProgrammer = {...person,...programmer}
+console.log(personAsProgrammer); */
+
+
