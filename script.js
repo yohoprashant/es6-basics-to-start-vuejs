b48dabc9f857cd37c0b3e5c5aea7986db076b160
Prashant <prash@qodemedia.com>
2019-04-10 21:22:44 +0545

    Code updated for CH7-Arrow-Functions

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-/* const myPromise = new Promise((resolve, reject) => {
-  if (Math.random() * 100 <= 90) {
-      resolve('Hello, Promises!');
-  }
-  reject(new Error('In 10% of the cases, I fail. Miserably.'));
-});
-
-myPromise.then((data)=>{
-  console.log(data);
-}).catch((error)=>{
-  console.log(error);
-}) */
-
-/* const getUser = new Promise(function(resolve, reject) {
-  setTimeout(function() {
-      const user = 'Summit'
-      if (user.length>4) {
-          resolve(user)
-      } else {
-          reject('Error!')
-      }
-  }, 1000)
-});
-function printUser(data) {
-  console.log(data)
+/* function square(num){
+  return num*num
 }
-console.log('hello');
-getUser.then(function(data) {
-  printUser(data)
-}).catch(function(error) {
-  console.log(error)
-})
-console.log('world'); */
+*/
 
+/* 
+const square = function(num){
+  return num*num;
+} 
+*/
+/* 
+const square = num => {
+  return num * num
+}
+*/
+/* console.log(square(3));  */
+
+
+/* 
+let result = [1, 2, 3, 4, 5].map( n => {
+  n = n % 3;
+  return n;
+});
+console.log(result); 
+*/
